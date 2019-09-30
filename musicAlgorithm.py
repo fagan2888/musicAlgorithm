b2bbdb22de329ca053976c7b79f2af4582346e7b
zhaoly12 <zhaoly12@163.com>
2019-09-30 15:40:34 +0800

    add python code

---
+++ musicAlgorithm.py
@@ -0,0 +1,22 @@
+
+# created by Zhao Liyuan
+
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from time import strftime
+from ipdb import set_trace
+import optparse
+
+class music():
+
+    def __init__():
+        self.
+
+    def music():
+        
+
+if __name__ == '__main__':
+    
+    music.music()
+
